53963309324d8ffa4df262f022c64ff5a83c5493
archengdev <106987393+archengdev@users.noreply.github.com>
2024-10-15 22:42:01 +0000

    initial commit

--- possible_solution.rb
+++ possible_solution.rb
@@ -14,7 +14,7 @@ puts "Checking the weather at #{user_location}...."
 
 # Get the lat/lng of location from Google Maps API
 
-gmaps_key = ENV.fetch("GMAPS_KEY")
+gmaps_key = ENV.fetch("google_maps_api_key")
 
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
 
---
+++ umbrella.rb
@@ -0,0 +1,5 @@
+require "http"
+require "json"
+
+
+gmaps_key = ENV["google_maps_api_key"]
